977f167a8a398415b69646517d235a73b5a22fcd
Harsh Varshney <hvarshney123@gmail.com>
2023-12-10 19:47:28 +0530

    Indentation fixed and unused terms removed/commented

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,33 +1,24 @@
 import React, { useState } from 'react'
 import SingleCommPost from '../components/SingleCommPost'
 import PoliceData from '../components/data'
-import { Button, useStatStyles } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 
 function Home() {
-  const [searchResults,setSearchResults]=useState([]);
-  const [searchTerm,setSearchTerm]=useState('');
-  
- 
+  // const [searchResults,setSearchResults]=useState([]);
+  // const [searchTerm,setSearchTerm]=useState('');
   return (
     <>
     <div className='w-[80%] mx-auto my-5 flex'>
-  <Input placeholder='Search Police Stations' />
-  <Button colorScheme='teal' >Search</Button>
+      <Input placeholder='Search Police Stations' />
+      <Button colorScheme='teal' >Search</Button>
   </div>
-   <div className='container w-[80%] mx-auto grid grid-cols-3 gap-10'>
-    
-    
-   {   PoliceData.map((item)=>{
-    return <SingleCommPost key={item.id} item={item}></SingleCommPost>
-   })}
-
-    
-    </div>
-
+  <div className='container w-[80%] mx-auto grid grid-cols-3 gap-10'>
+    {PoliceData.map((item)=>{
+      return <SingleCommPost key={item.id} item={item}></SingleCommPost>
+    })}
+  </div>
     </>
-    
-  
   )
 }
 
--- src/components/data.js
+++ src/components/data.js
@@ -3,7 +3,7 @@ const PoliceData=[{
     "name":"SURAJGARH POLICE STATION",
     "adress":"xyz 45 ok",
     "rating":3,
-     "image":"https://images.unsplash.com/photo-1600081191763-05da665acf1a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9saWNlJTIwc3RhdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
+    "image":"https://images.unsplash.com/photo-1600081191763-05da665acf1a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9saWNlJTIwc3RhdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
 
 
 },
@@ -12,7 +12,7 @@ const PoliceData=[{
     "name":"BURAJGARH POLICE STATION",
     "adress":"xyz 45 ok",
     "rating":3,
-     "image":"https://images.unsplash.com/photo-1600081191763-05da665acf1a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9saWNlJTIwc3RhdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
+    "image":"https://images.unsplash.com/photo-1600081191763-05da665acf1a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9saWNlJTIwc3RhdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
 
 
 },
@@ -21,7 +21,7 @@ const PoliceData=[{
     "name":"SURAJGARH POLICE STATION",
     "adress":"xyz 45 ok",
     "rating":3,
-     "image":"https://images.unsplash.com/photo-1600081191763-05da665acf1a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9saWNlJTIwc3RhdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
+    "image":"https://images.unsplash.com/photo-1600081191763-05da665acf1a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9saWNlJTIwc3RhdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
 
 
 },
@@ -30,7 +30,7 @@ const PoliceData=[{
     "name":"SURAJGARH POLICE STATION",
     "adress":"xyz 45 ok",
     "rating":3,
-     "image":"https://images.unsplash.com/photo-1600081191763-05da665acf1a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9saWNlJTIwc3RhdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
+    "image":"https://images.unsplash.com/photo-1600081191763-05da665acf1a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9saWNlJTIwc3RhdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
 
 
 }
