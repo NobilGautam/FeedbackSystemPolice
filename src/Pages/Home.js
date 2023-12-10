175ba7417b826135a0d5895b919c4394da41e248
divyammishra7 <111589847+divyammishra7@users.noreply.github.com>
2023-12-10 17:04:03 +0530

    rating added (#5)
    * rating added
    
    * search half done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SingleCommPost from '../components/SingleCommPost'
 import PoliceData from '../components/data'
+import { Button, useStatStyles } from '@chakra-ui/react'
+import { Input } from '@chakra-ui/react'
 
 function Home() {
+  const [searchResults,setSearchResults]=useState([]);
+  const [searchTerm,setSearchTerm]=useState('');
+  
  
   return (
-    <div className='container w-[80%] mx-auto grid grid-cols-3 gap-10'>
-   {PoliceData.map((item)=>{
+    <>
+    <div className='w-[80%] mx-auto my-5 flex'>
+  <Input placeholder='Search Police Stations' />
+  <Button colorScheme='teal' >Search</Button>
+  </div>
+   <div className='container w-[80%] mx-auto grid grid-cols-3 gap-10'>
+    
+    
+   {   PoliceData.map((item)=>{
     return <SingleCommPost key={item.id} item={item}></SingleCommPost>
    })}
 
     
     </div>
+
+    </>
+    
+  
   )
 }
 
--- src/components/data.js
+++ src/components/data.js
@@ -9,7 +9,7 @@ const PoliceData=[{
 },
 {
     "id":2,
-    "name":"SURAJGARH POLICE STATION",
+    "name":"BURAJGARH POLICE STATION",
     "adress":"xyz 45 ok",
     "rating":3,
      "image":"https://images.unsplash.com/photo-1600081191763-05da665acf1a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cG9saWNlJTIwc3RhdGlvbnxlbnwwfHwwfHx8MA%3D%3D",
