305cb79e919102d0fd5326044216a0b203b0fe64
Divyam Mishra <divyammishra0918@gmail.com>
2023-12-11 21:59:18 +0530

    check

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,7 +46,7 @@ return (
     <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
       {/* {
         Links.map((link)=>(
-          // Render values according to login state
+          .....// Render values according to login state
           <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
             <a href={link.link} onClick={link.name=='LOGIN'?signIN:""}className='text-gray-800 hover:text-gray-400 duration-500'>{link.name}</a>
           </li>
