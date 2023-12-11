602ab68d6f3ab20cde39c41cb894917cd926f070
Divyam Mishra <divyammishra0918@gmail.com>
2023-12-11 13:06:18 +0530

    minor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ function Navbar() {
     signInWithPopup(Auth,Provider)
     .then(res=>{console.log("ok")})
     .catch(err=>{console.log(err)});
+    navigator('/');
   }
   const signout=async()=>{
     await signOut(Auth);
