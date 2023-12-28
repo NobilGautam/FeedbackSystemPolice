a32abd7c8a2871d7c2e50f24ab3f85f0906bf8c7
Divyam Mishra <divyammishra0918@gmail.com>
2023-12-29 00:41:25 +0530

    loader changed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ import { Button } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 import InfiniteScroll from 'react-infinite-scroller';
 import { Circles } from 'react-loader-spinner'
-
+import { ThreeDots } from 'react-loader-spinner'
 
 
 function Home() {
@@ -13,14 +13,18 @@ function Home() {
   const [searchTerm, setSearchTerm] = useState('');
   const [index, setIndex] = useState(15);
   const [flag,setFlag]=useState(true);
+  const [loader,setLoader]=useState(false);
   const url = "https://script.google.com/macros/s/AKfycbzvSyrLrcpMXul2X517D714Y8LvOlr29FDmJNH0OYDCqBEqKPPts9VBw6pIrjCORYB6uQ/exec"
   const loadFunc = () => {
     // setIndex(index+3);
+    setLoader(true);
+    
     setTimeout(() => {
       setIndex(index => index + 6);
 
-    }, 1500);
+    }, 2000);
 
+    setLoader(false);
 
 
   }
@@ -87,16 +91,16 @@ setFlag(!flag);
 
         loadMore={loadFunc}
         hasMore={true || false}
-        loader={searchResults.length > 15 ? <h1 className='text-center w-[100%] flex justify-center mx-auto'><Circles
-          height="80"
-          width="80"
-          color="#4fa94d"
-          ariaLabel="circles-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-          visible={true}
-
-        /></h1> : ""}
+        loader={searchResults.length > 15 ? <h1 className='text-center w-[100%] flex justify-center mx-auto'><ThreeDots
+        visible={true}
+        height="100"
+        width="100"
+        color="#8C4E1D"
+        radius="9"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        /></h1>: ""}
 
       >
         <div className='container w-[80%] mx-auto grid md:grid-cols-3 grid-cols-1 gap-10 mt-5'>
@@ -105,6 +109,8 @@ setFlag(!flag);
           {searchResults.slice(0, Math.min(index, searchResults.length)).map((item) => {
             return <SingleCommPost key={item.id} item={item}></SingleCommPost>
           })}
+          
+          <Button onClick={loadFunc}>Load More</Button>
         </div>
       </InfiniteScroll>
     </div>
