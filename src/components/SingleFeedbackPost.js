aae13d96888068cebff2471e60f751a19e08692f
Nobil Gautam <nobilgautam007@gmail.com>
2023-12-29 15:52:16 +0530

    SingleFeedbackPost made responsive

--- src/components/SingleFeedbackPost.js
+++ src/components/SingleFeedbackPost.js
@@ -12,20 +12,20 @@ function SingleFeedbackPost({ item, ImgLinks, addressLinks }) {
 
   return (
     <>
-    <div className='flex justify-center my-4 lg:h-[140px] hover:scale-105 duration-300'>
-    <div className='flex flex-row md:w-[80%] justify-between rounded-2xl overflow-hidden shadow-md shadow-[#8e8e8e] bg-[#f6bd85]'>
-      <div className='flex justify-between items-center w-[30%]'>
-        <img src={ImgLinks.get(item.PoliceStation)} className='w-[60%] h-[100%]' alt={`${item.name} Police Station Picture`}></img>
-        <h2 className='text-[1.2rem] font-bold text-center w-[100%]'>{item.PoliceStation}</h2>
+    <div className='flex justify-center my-4 lg:h-[140px] lg:hover:scale-105 duration-300'>
+      <div className='flex lg:flex-row w-[80%] flex-col lg:justify-between rounded-2xl overflow-hidden shadow-md shadow-[#8e8e8e] bg-[#f6bd85]'>
+        <div className='flex lg:flex-row flex-col w-[100%] lg:justify-between lg:items-center lg:w-[30%]'>
+          <img src={ImgLinks.get(item.PoliceStation)} className='lg:w-[60%] lg:h-[100%]' alt={`${item.name} Police Station Picture`}></img>
+          <h2 className='text-[1.2rem] font-bold text-center w-[100%]'>{item.PoliceStation}</h2>
+        </div>
+        <div className='flex text-center items-center lg:text-[1.2rem] font-light justify-center lg:w-[40%]'>
+          {addressLinks.get(item.PoliceStation)}
+        </div>
+    
+        <div className={`flex items-center bg-[#f9d4ae] m-2 rounded-lg p-2 font-light lg:w-[30%] justify-center ${item.Feel<=0?'text-red-500':'text-green-500'}`}>
+          {item.Feedback}
+        </div>
       </div>
-      <div className='flex items-center md:text-[1.2rem] font-light justify-center w-[40%]'>
-        {addressLinks.get(item.PoliceStation)}
-      </div>
- 
-      <div className={`flex items-center bg-[#f9d4ae] m-2 rounded-lg p-2 font-light w-[30%] justify-center ${item.Feel<=0?'text-red-500':'text-green-500'}`}>
-        {item.Feedback}
-      </div>
-    </div>
     </div>
     </>
   )
