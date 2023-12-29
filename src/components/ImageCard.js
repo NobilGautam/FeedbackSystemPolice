65ed98c464d42e9588ba275af92613c3d92fd104
Harsh Varshney <hvarshney123@gmail.com>
2023-12-29 20:20:05 +0530

    aspect ratio fixed for mobile

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -9,7 +9,7 @@ const ImageCard = ({ children, ImgSrc, ...props }) => {
         <img
           src={ImgSrc}
           alt="Police Pic"
-          className="transition-transform aspect-[4/3] w-full object-cover group-hover:scale-110 duration-200"
+          className="transition-transform aspect-video md:aspect-[4/3] w-full object-cover group-hover:scale-110 duration-200"
         />
         <div className="absolute inset-0 flex items-end bg-gradient-to-t from-black/90 to-transparent">
           <div style={{ display: 'flex', width: '100%' }} className="p-5 text-white">{children}</div>
