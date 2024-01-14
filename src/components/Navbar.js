ccdf7aa241b25e248f2bdde6885d48e125c3c762
Iron0utlaw <hvarshney123@gmail.com>
2024-01-14 16:01:17 +0530

    Navbar menu mst

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,21 @@ import { signInWithPopup, signOut } from "firebase/auth";
 import { Link } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
-import close from "../assets/close.svg";
-import menu from "../assets/menu.svg";
 import rpLogo from "../assets/rplogo.png";
 import emblem from "../assets/emblem.png";
+import {
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuGroup,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 function Navbar() {
   const navigator = useNavigate();
-  const [open, setOpen] = useState(false);
   const [user] = useAuthState(Auth);
-
-  const [showTooltip, setShowTooltip] = useState(false);
-
-  const handleImageClick = () => {
-    setShowTooltip(!showTooltip);
-  };
 
   const signIN = () => {
     signInWithPopup(Auth, Provider)
@@ -43,6 +43,7 @@ function Navbar() {
   const handleNavClick = (link) => {
     setSelectedLink(link.name);
   };
+
   return (
     <div className="shadow-md w-full fixed z-20 top-0 left-0">
       <div className="md:flex items-center justify-between py-4 md:px-10 px-7 bg-[#8C4E1D]">
@@ -53,96 +54,68 @@ function Navbar() {
             Rajasthan Police Feedback
           </span>
           <div className="md:hidden flex flex-1 justify-end items-center">
-            <img
-              src={open ? close : menu}
-              alt="menu"
-              className="w-[28px] h-[28px] object-contain cursor-pointer"
-              onClick={() => setOpen(!open)}
-            />
-            <div
-              className={`${
-                !open ? "hidden" : "flex"
-              } bg-slate-50 p-6 absolute top-20 right-0 mx-4 shadow-2xl my-2 min-w-[140px] z-10 rounded-xl`}
-            >
-              <ul className="list-none flex justify-end items-start flex-1 flex-col gap-2">
-                {user ? (
-                  <li
-                    onClick={() => {
-                      setOpen(!open);
-                    }}
-                    className="md:ml-8 text-base md:my-0 my-3 flex items-center justify-between h-min"
-                  >
-                    <div>
-                      {user.displayName.split(" ").map((xname) => (
-                        <h1>{xname}</h1>
-                      ))}
-                    </div>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={
+                  user ? (
                     <img
                       src={user.photoURL}
-                      className="text-sm rounded-[50%] w-[30%] logoimg "
+                      className="rounded-full"
                       alt={user.displayName}
+                      style={{
+                        width: "36px",
+                        height: "36px",
+                        objectFit: "cover", 
+                      }}
                     />
-                  </li>
+                  ) : (
+                    <HamburgerIcon />
+                  )
+                }
+                style={{
+                  backgroundColor: "#F0F0F0", 
+                  borderRadius: "50%",
+                }}
+              />
+              <MenuList>
+                {user ? (
+                  <MenuGroup title={`Namaste, ${user.displayName}`} />
                 ) : (
-                  <></>
+                  <MenuItem>
+                    <Link className="text-sm text-green-600" onClick={signIN}>
+                      Login
+                    </Link>
+                  </MenuItem>
                 )}
                 {Links.filter((link) => link).map((link) => (
-                  <li
-                    key={link.name}
-                    onClick={() => {
-                      setOpen(!open);
-                      handleNavClick(link);
-                    }}
-                    className="md:ml-8  text-base md:my-0 my-3"
-                  >
+                  <MenuItem key={link.name}>
                     <Link
+                      as={Link}
                       to={link.link}
-                      className="text-black hover:text-gray-400 duration-500"
+                      className="text-sm"
                       onClick={() => {
-                        setOpen(!open);
+                        handleNavClick(link);
                       }}
                     >
-                      <p
-                        className={`hover:border-b-[3px] ${
-                          selectedLink === link.name ? "border-b-[3px]" : ""
-                        } border-black duration-100 pb-2`}
-                      >
-                        {link.name}
-                      </p>
+                      {link.name}
                     </Link>
-                  </li>
+                  </MenuItem>
                 ))}
                 {user ? (
-                  <li
-                    onClick={() => {
-                      setOpen(!open);
-                    }}
-                    className="md:ml-8  text-base md:my-0 my-3 flex items-center"
-                  >
+                  <MenuItem>
                     <Link
                       onClick={signout}
-                      className="text-red-500 hover:text-gray-400 duration-500   "
+                      className="text-red-500 text-sm hover:text-gray-400 duration-500"
                     >
                       Logout
                     </Link>
-                  </li>
+                  </MenuItem>
                 ) : (
-                  <li
-                    onClick={() => {
-                      setOpen(!open);
-                    }}
-                    className="md:ml-8  text-base md:my-0 my-3"
-                  >
-                    <Link
-                      onClick={signIN}
-                      className="text-black hover:text-gray-400 duration-500"
-                    >
-                      Login
-                    </Link>
-                  </li>
+                  <></>
                 )}
-              </ul>
-            </div>
+              </MenuList>
+            </Menu>
           </div>
         </div>
         <ul className="hidden font-medium flex-row md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-[#8C4E1D] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in">
@@ -174,24 +147,46 @@ function Navbar() {
             </li>
           ))}
           {user ? (
-            <li className="md:ml-8 text-xl md:my-0 my-7 flex items-center">
-              <img
-                src={user.photoURL}
-                className="text-sm rounded-[50%] w-[50%] hover:shadow-[#B27D3E] hover:shadow-lg cursor-pointer"
-                alt={user.displayName}
-                onClick={handleImageClick}
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                className="ml-5"
+                icon={
+                  <img
+                    src={user.photoURL}
+                    className="rounded-full"
+                    alt={user.displayName}
+                    style={{
+                      width: "40px",
+                      height: "40px", 
+                      objectFit: "cover",
+                    }}
+                  />
+                }
+                style={{
+                  backgroundColor: "#F0F0F0", 
+                  borderRadius: "50%",
+                }}
+                size="lg"
               />
-              {showTooltip && (
-                <div className="bg-slate-50 px-8 py-2 absolute top-20 right-0 mx-20 shadow-2xl my-2 w-min z-10 rounded-xl">
+              <MenuList>
+                {user ? (
+                  <MenuGroup title={`Namaste, ${user.displayName}`} />
+                ) : (
+                  <MenuItem>
+                    <Link onClick={signIN}>Login</Link>
+                  </MenuItem>
+                )}
+                <MenuItem>
                   <Link
                     onClick={signout}
                     className="text-red-500 hover:text-gray-400 duration-500"
                   >
                     Logout
                   </Link>
-                </div>
-              )}
-            </li>
+                </MenuItem>
+              </MenuList>
+            </Menu>
           ) : (
             <li className="md:ml-8 text-xl md:my-0 my-7">
               <Link
