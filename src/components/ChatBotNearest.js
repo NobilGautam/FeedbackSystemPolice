48afc9e0ac0107dfa00ff53cbca40eeee6024115
Divyam Mishra <divyammishra0918@gmail.com>
2024-01-16 23:56:18 +0530

    chatbot

---
+++ src/components/ChatBotNearest.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ChatBotNearest({previousStep}) {
+    console.log(previousStep.message);
+  return (
+    <div>ChatBotNearest</div>
+  )
+}
+
+export default ChatBotNearest
