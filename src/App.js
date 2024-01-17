130d3ef920a66d0273054ea994f504ad934d1ace
Iron0utlaw <hvarshney123@gmail.com>
2024-01-17 18:51:54 +0530

    gri fetched and update 2

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ import IntialLoader from "./components/IntialLoader";
 import Grievance from "./Pages/Grievance";
 import MyGri from "./components/MyGri";
 import Admin from "./Pages/Admin";
-import  SingleAdmin from "./Pages/SingleAdmin"
+import SingleAdmin from "./Pages/SingleAdmin";
 import { useSupabase } from "./context/SupabaseContext";
 
 function App() {
@@ -49,7 +49,7 @@ function App() {
   }, []);
 
   const { t } = useTranslation();
-  const {admin}=useSupabase();
+  const { admin } = useSupabase();
 
   return (
     <div>
@@ -58,85 +58,102 @@ function App() {
       ) : (
         <>
           <Navbar />
-
-      <ChatBot steps={steps} floating={true} className="chatbot" />
-      <Button
-        className="customButton helpline"
-        onClick={() => {
-          setOverlay(<OverlayOne />);
-          onOpen();
-        }}
-      >
-        <IoCallOutline />
-        <span className="ml-2">{t("navbar.helplines")}</span>
-      </Button>
-      <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        {user && <Route path="/myfeedback" element={<Feedback />}></Route>}
-        {user && <Route path="/myfeedback/:documentID" element={<Feedback />}></Route>}
-        <Route path="/single/:id" element={<Single />}></Route>
-        {user && <Route path="/myVisits" element={<MyVisits />}></Route>}
-        {user && <Route path="/newFeedback/:documentId" element={<Form />} />}
-        <Route path="/newVisit" element={<NewVisit />} />
-        <Route path="/QR/:id" element={<QR />} />
-        {user && <Route path="/form" element={<Form />}></Route>}
-        {user && <Route path="/followupform/:documentID" element={<FollowupForm />}></Route>}
-        { <Route path="/followupform" element={<FollowupForm />}></Route>}
-        {user && <Route path="/grievance" element={<Grievance/>}></Route>}
-        {user && <Route path="/admin" element={<Admin/>}></Route>}
-        {user && <Route path="/singleAdmin/:pincode" element={<SingleAdmin admin={admin}/>}></Route>}
-        {/* { user && <Route path="/chat" element={<Chat/>}></Route>}
-         */}
-        <Route path="*" element={<Error />}></Route>
-      </Routes>
-      <Modal isCentered isOpen={isOpen} onClose={onClose}>
-        {overlay}
-        <ModalContent className="border-solid border-2 border-[#8C4E1D]">
-          <ModalHeader className="bg-[#8C4E1D] flex justify-between">
-            <h1 className="text-2xl text-white font-sans font-semibold">
-              Rajasthan Police Helplines
-            </h1>
-            <Button className="customButton" onClick={onClose}>
-              <IoClose/>
-            </Button>
-          </ModalHeader>
-          <ModalBody>
-            <Text>
-              {helplines.map((item, index) => {
-                return (
-                  <div key={index} className="py-2">
-                    <h2 className="text-[#8C4E1D] font-sans font-bold text-md">
-                      {t("modal."+item.name)}:&nbsp;
-                      <>
-                        <a
-                          href={"tel:" + item.number}
-                          className="text-blue-500 underline"
-                        >
-                          {item.number}
-                        </a>
-                      </>
-                      {item.number2 && (
-                        <>
-                          {" "}
-                          /{" "}
-                          <a
-                            href={"tel:" + item.number}
-                            className="text-blue-500 underline"
-                          >
-                            {item.number}
-                          </a>
-                        </>
-                      )}
-                    </h2>
-                  </div>
-                );
-              })}
-            </Text>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-      </>
-}
+          <ChatBot steps={steps} floating={true} className="chatbot" />
+          <Button
+            className="customButton helpline"
+            onClick={() => {
+              setOverlay(<OverlayOne />);
+              onOpen();
+            }}
+          >
+            <IoCallOutline />
+            <span className="ml-2">{t("navbar.helplines")}</span>
+          </Button>
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            {user && <Route path="/myfeedback" element={<Feedback />}></Route>}
+            {user && (
+              <Route
+                path="/myfeedback/:documentID"
+                element={<Feedback />}
+              ></Route>
+            )}
+            <Route path="/single/:id" element={<Single />}></Route>
+            {user && <Route path="/myVisits" element={<MyVisits />}></Route>}
+            {user && (
+              <Route path="/newFeedback/:documentId" element={<Form />} />
+            )}
+            {user && <Route path="/admin" element={<Admin />}></Route>}
+            {user && (
+              <Route
+                path="/singleAdmin/:pincode"
+                element={<SingleAdmin admin={admin} />}
+              ></Route>
+            )}
+            <Route path="/newVisit" element={<NewVisit />} />
+            <Route path="/QR/:id" element={<QR />} />
+            {user && <Route path="/form" element={<Form />}></Route>}
+            {user && (
+              <Route
+                path="/followupform/:documentID"
+                element={<FollowupForm />}
+              ></Route>
+            )}
+            {<Route path="/followupform" element={<FollowupForm />}></Route>}
+            {user && <Route path="/grievance" element={<Grievance />}></Route>}
+            {/* { user && <Route path="/chat" element={<Chat/>}></Route>}
+             */}
+            <Route path="*" element={<Error />}></Route>
+            <Route path="/myGri" element={<MyGri />}></Route>
+          </Routes>
+          <Modal isCentered isOpen={isOpen} onClose={onClose}>
+            {overlay}
+            <ModalContent className="border-solid border-2 border-[#8C4E1D]">
+              <ModalHeader className="bg-[#8C4E1D] flex justify-between">
+                <h1 className="text-2xl text-white font-sans font-semibold">
+                  Rajasthan Police Helplines
+                </h1>
+                <Button className="customButton" onClick={onClose}>
+                  <IoClose />
+                </Button>
+              </ModalHeader>
+              <ModalBody>
+                <Text>
+                  {helplines.map((item, index) => {
+                    return (
+                      <div key={index} className="py-2">
+                        <h2 className="text-[#8C4E1D] font-sans font-bold text-md">
+                          {t("modal." + item.name)}:&nbsp;
+                          <>
+                            <a
+                              href={"tel:" + item.number}
+                              className="text-blue-500 underline"
+                            >
+                              {item.number}
+                            </a>
+                          </>
+                          {item.number2 && (
+                            <>
+                              {" "}
+                              /{" "}
+                              <a
+                                href={"tel:" + item.number}
+                                className="text-blue-500 underline"
+                              >
+                                {item.number}
+                              </a>
+                            </>
+                          )}
+                        </h2>
+                      </div>
+                    );
+                  })}
+                </Text>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </>
+      )}
     </div>
   );
 }
